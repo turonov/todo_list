4abf0705d8906b63874f8f1f36e9584ed5502f0d
ZARIFMAC <uzbzarif@gmail.com>
2023-03-23 10:06:20 +0500

    get users

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+# Import user model
+from django.contrib.auth.models import User
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        user = User.objects.all()
+        data = {
+            'users': []
+        }
+        for i in user:
+            data['users'].append({
+                'id': i.id,
+                'username': i.username,
+                'email': i.email
+            })
+        return JsonResponse(data)
+    def post(self, request):
+        return JsonResponse({'message': 'POST'})
+    
