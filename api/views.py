0cae02074e996c0f35b4289237f266ee8fa50da4
ZARIFMAC <uzbzarif@gmail.com>
2023-03-23 10:52:11 +0500

    user authentication

--- api/views.py
+++ api/views.py
@@ -3,6 +3,10 @@ from django.views import View
 # Import user model
 from django.contrib.auth.models import User
 
+# Import authentication classes
+from django.contrib.auth import authenticate
+
+
 class HomeView(View):
     def get(self, request):
         user = User.objects.all()
@@ -18,4 +22,17 @@ class HomeView(View):
         return JsonResponse(data)
     def post(self, request):
         return JsonResponse({'message': 'POST'})
+
+
+def index(request):
+    # Check if user is authenticated
+    user = authenticate(username='user', password='789987qwe')
+    if user is not None:
+        return JsonResponse({
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+        })
+    return JsonResponse({'result': 'You are not authenticated'})
     
--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from api.views import HomeView
+from api.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view()),
+    path('index/', index)
 ]
