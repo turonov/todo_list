3ca08c9a77395cac51e0dbdcbeff490e112cd4e0
ZARIFMAC <uzbzarif@gmail.com>
2023-03-28 09:53:25 +0500

    model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    task = models.CharField(max_length=50)
+    description = models.TextField()
+    complited = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
