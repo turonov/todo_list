4e0e5c7dee5636345d7995e2d41f516290142439
ZARIFMAC <uzbzarif@gmail.com>
2023-03-28 10:07:07 +0500

    isAuth

--- api/views.py
+++ api/views.py
@@ -1,35 +1,15 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse,HttpRequest
 from django.views import View
 # Import user model
 from django.contrib.auth.models import User
-
+from .models import Task, Student
 # Import authentication classes
 from django.contrib.auth import authenticate
 from base64 import b64decode
 
-class HomeView(View):
-    def get(self, request):
-        user = User.objects.all()
-        data = {
-            'users': []
-        }
-        for i in user:
-            data['users'].append({
-                'id': i.id,
-                'username': i.username,
-                'email': i.email
-            })
-        return JsonResponse(data)
-    def post(self, request):
-        return JsonResponse({'message': 'POST'})
-
-
-def index(request):
-    # Get headers
-    auth = request.headers.get('Authorization')
-    # Check if Authorization header is present
+def isAuth(auth):
     if auth is None:
-        return JsonResponse({'result': 'You are not authenticated'})
+        return False
     # Get token
     token = auth.split(' ')[1]
     auth=b64decode(token).decode() # Decode token
@@ -40,11 +20,43 @@ def index(request):
     user = authenticate(username=username, password=password)
 
     if user is not None:
-        return JsonResponse({
-            'username': user.username,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'email': user.email,
-        })
-    return JsonResponse({'result': 'You are not authenticated'})
-    
+        return True
+    return False
+
+
+
+def tasks(request: HttpRequest) -> JsonResponse:
+    """
+    Create a task
+    """
+    auth = request.headers.get('Authorization')
+    if isAuth(auth):
+        if request.method == 'POST':
+            return JsonResponse({'message': 'Task created successfully'}, status=201)
+        if request.method == 'GET':
+            todos = {
+                'todo':[]
+            }
+            # Get all tasks
+            tasks = Task.objects.all()
+            for task in tasks:
+                todos['todo'].append({
+                    'id': task.id,
+                    'task': task.task,
+                    'description': task.description,
+                    'complited': task.complited,
+                    'created_at': task.created_at,
+                    'updated_at': task.updated_at,
+                   
+                })
+
+            return JsonResponse(todos, status=200)
+        
+    else:
+        return JsonResponse({'message': 'Unauthorized'}, status=401)
+
+            
+
+
+
+        
