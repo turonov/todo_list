7218023baca9e01230bbb5cc68a1c56c993f10b5
ZARIFMAC <uzbzarif@gmail.com>
2023-03-28 10:16:08 +0500

    User model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+
 
 
 class Task(models.Model):
@@ -12,4 +10,4 @@ class Task(models.Model):
     complited = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
